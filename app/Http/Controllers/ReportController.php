e986581a3a83b71b77ffef303625f39f4128db9b
smoook92 <axess-88@mail.ru>
2023-11-20 23:28:54 +0300

    Сделал отчет на русском языке

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -13,41 +13,38 @@ class ReportController extends Controller
     {
         $payment = Payment::find($pid);
         $pdf = App::make('dompdf.wrapper');
-        $print = "<div style='margin:20px; padding:20px;>";
-        $print.= "<h1 align='center'>Квитанция об оплате</h1>";
-        $print.= "<hr />";
-        $print.= "<p> Receipt number: <b>" . $pid . "</b> </p>";
-        $print.= "<p> Registration number: <b>" . $payment->paid_date . "</b></p>";
+        $print = "<div style='margin:20px; padding:20px;'>";
+        $print .= '<h1 align="center" style="font-family: DejaVu Sans, sans-serif;">Квитанция об оплате</h1>';
 
-        $print.= "<p> Student Name: <b>" . $payment->enrollment->student->name . "</b></p>";
+        $print .= "<hr />";
+        $print .= '<p style="font-family: firefly, DejaVu Sans, sans-serif;">Номер квитанции: <b>' . $pid . '</b></p>';
+        $print .= '<p style="font-family: firefly, DejaVu Sans, sans-serif;">Регистрационный номер: <b>' . $payment->paid_date . '</b></p>';
 
-        $print.= "<hr/>";
-        $print.= "<table style='width: 100%'>";
+        $print .= '<p style="font-family: firefly, DejaVu Sans, sans-serif;">Имя Студента: <b>' . $payment->enrollment->student->name . '</b></p>';
+
+        $print .= "<hr/>";
+        $print .= "<table style='width: 100%'>";
 
         // Создание таблицы отчета
-        $print.= "<tr>";
-        $print.= "<td>Description</td>";
-        $print.= "<td>Price</td>";
+        $print .= "<tr>";
+        $print .= '<td style="font-family: firefly, DejaVu Sans, sans-serif;">Описание</td>';
+        $print .= '<td style="font-family: firefly, DejaVu Sans, sans-serif;">Цена</td>';
+        $print .= "</tr>";
 
-        $print.= "</tr>";
+        $print .= "<tr>";
+        $print .= '<td> <h3 style="font-family: firefly, \'DejaVu Sans\', sans-serif;">' . $payment->enrollment->batch->name . '</h3> </td>';
+        $print .= '<td> <h3 style="font-family: firefly, \'DejaVu Sans\', sans-serif;">' . $payment->amount . ' ₽</h3> </td>';
+        $print .= "</tr>";
 
-        $print.= "<tr>";
-        $print.= "<td> <h3>" . $payment->enrollment->batch->name . "</h3> </td>";
-        $print.= "<td> <h3>" . $payment->amount . "</h3> </td>";
-        $print.= "<tr>";
+        $print .= "</table>";
+        $print .= "<hr/>";
 
-        $print.= "</table>";
-        $print.= "<hr/>";
+        $print .= '<span style="font-family: firefly, \'DejaVu Sans\', sans-serif;"> Заказчик: <b>' . (Auth::check() ? Auth::user()->name : 'Неизвестный') . '</b></span>';
+        $print .= '<span style="font-family: firefly, \'DejaVu Sans\', sans-serif;"> Дата печати: <b>' . date('Y-m-d') . '</b></span>';
 
-        $print.= "<span> Printed: <b>" . (Auth::check() ? Auth::user()->name : 'Unknown') . "</b></span>";
-        $print.= "<span> Print date: <b>". date('Y-m-d') . "</b></span>";
-
-        $print.= "</div>";
+        $print .= "</div>";
         $pdf->loadHTML($print);
-
         return $pdf->stream();
     }
-  
 }
-
 ?>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,11 +2,12 @@
 <html lang="ru">
 
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <title>Система управления студентами</title>
   <style>
+    body { font-family: DejaVu Sans, sans-serif; }
     /* The side navigation menu */
 .sidebar {
   margin: 0;
